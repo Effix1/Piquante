32747d1563653a8ec840749b0e2693f194841bf8
Effix <107648239+Effix1@users.noreply.github.com>
2023-03-18 17:07:44 +0100

    insertion route auth plus token

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,8 @@ const mongoose = require ('mongoose');
 const userRoutes = require ('./routes/user')
 
 mongoose.connect('mongodb+srv://Effix1st:Eloise2015@cluster0.oljcbaq.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+useNewUrlParser: true,
+useUnifiedTopology: true
 })
 .then(()=>console.log('Connexion à mongoDB réussie ! '))
 .catch(()=>console.log('Connexion à mongoDB échoué ! '));
@@ -21,9 +21,6 @@ app.use((req,res,next)=>{
 
 app.use(bodyParser.json())
 
-app.use('/api/auth/signup', userRoutes)
-app.use('/api/auth/login', userRoutes)
-
-
+app.use('/api/auth', userRoutes)
 
 module.exports=app;
--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User')
 
@@ -10,31 +11,38 @@ exports.signup = (req,res,next)=>{
             password: hash
         });
         user.save()
-            .then(()=>res.status(201).json({message:'Utilisateur crée'}))
-            .catch(error=> res.status(400).json({error}))
+        .then(()=>res.status(201).json({message:'Utilisateur crée'}))
+        .catch(error=> res.status(400).json({error}))
     })
     .catch(error=> res.status(500).json({error}))
 };
-
 exports.login = (req,res,next)=>{
     User.findOne({email: req.body.email})
     .then(user =>{
-        if(user===nul){
+        if(user===null){
             res.status(401).json({message: 'paire identifiant mot de passe incorrecte'})
         } else {
             bcrypt.compare(req.body.password, user.password)
             .then(valid =>{
                 if(!valid){
                     res.status(401).json({message:'paire identifiant mot de passe incorrecte'})
-                } else {
-                    res.status(200).json({
-                        userId:user._id,
-                        token: 'TOKEN'
-                    })
-                }
-            })
-            .catch(error=>res.status(500).json({error}));
-        }
-    })
-    .catch(error=>res.status(500).json({error}))
-};
+                } 
+                res.status(200).json({
+                    userId:user._id,
+                    token: jwt.sign(
+                        { userId: user._id},
+                        'RANDOM_TOKEN_SECRET',
+                        {expiresIn:'24h'}
+                        )
+                    });
+                    
+                })
+                .catch(error=> {
+                    res.status(500).json({error})
+                });
+            }
+        })
+        .catch(error=> {
+            res.status(500).json({error})
+        })
+    };
