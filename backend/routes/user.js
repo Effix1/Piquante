b0cde0d132734d35f52012fa7b0b3de0907229d6
Effix <107648239+Effix1@users.noreply.github.com>
2023-03-20 14:33:07 +0100

    insertion rout + controllers + schema sauces

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login',  userCtrl.login);
 module.exports = router;
